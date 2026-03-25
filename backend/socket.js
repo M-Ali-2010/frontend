69fe48979e3cbf1a8442b823d5202b4550a62131
Muhammad_Ali <alimuhammad2010267@gmail.com>
2026-03-25 18:25:52 +0500

    remove backend

--- backend/socket.js
+++
@@ -1,9 +0,0 @@
-// backend/socket.js
-function setupSocket(io) {
-  io.on('connection', (socket) => {
-    // No auth required for broadcast; frontend can still fetch JWT for protected actions.
-    socket.on('disconnect', () => {});
-  });
-}
-
-module.exports = { setupSocket };
